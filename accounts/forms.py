e4120b1ad375d4d3f8c6ad944883a3e48e81bef2
Vivek Narula <narula.vivek@gmail.com>
2025-12-27 15:10:24 +0530

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,36 @@ from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
 
-from .models import User, BankAccountType, UserBankAccount
+from .models import (
+    User,
+    BankAccountType,
+    UserBankAccount,
+    UserAddress,
+)
 from .constants import GENDER_CHOICE
+
+
+class UserAddressForm(forms.ModelForm):
+    class Meta:
+        model = UserAddress
+        fields = [
+            "street_address",
+            "city",
+            "postal_code",
+            "country",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                "class": (
+                    "appearance-none block w-full bg-gray-200 "
+                    "text-gray-700 border border-gray-200 rounded "
+                    "py-3 px-4 leading-tight focus:outline-none "
+                    "focus:bg-white focus:border-gray-500"
+                )
+            })
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -25,42 +53,42 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = [
-            'first_name',
-            'last_name',
-            'email',
-            'account_type',   # ✅ ADD
-            'gender',         # ✅ ADD
-            'birth_date',     # ✅ ADD
-            'password1',
-            'password2',
+            "first_name",
+            "last_name",
+            "email",
+            "account_type",
+            "gender",
+            "birth_date",
+            "password1",
+            "password2",
         ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs.update({
-                'class': (
-                    'appearance-none block w-full bg-gray-200 '
-                    'text-gray-700 border border-gray-200 '
-                    'rounded py-3 px-4 leading-tight '
-                    'focus:outline-none focus:bg-white '
-                    'focus:border-gray-500'
+                "class": (
+                    "appearance-none block w-full bg-gray-200 "
+                    "text-gray-700 border border-gray-200 "
+                    "rounded py-3 px-4 leading-tight "
+                    "focus:outline-none focus:bg-white "
+                    "focus:border-gray-500"
                 )
             })
 
     @transaction.atomic
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.username = None  # since email is USERNAME_FIELD
+        user.username = None  # email is USERNAME_FIELD
 
         if commit:
             user.save()
 
             UserBankAccount.objects.create(
                 user=user,
-                account_type=self.cleaned_data['account_type'],
-                gender=self.cleaned_data['gender'],
-                birth_date=self.cleaned_data.get('birth_date'),
+                account_type=self.cleaned_data["account_type"],
+                gender=self.cleaned_data["gender"],
+                birth_date=self.cleaned_data.get("birth_date"),
                 account_no=user.id + settings.ACCOUNT_NUMBER_START_FROM,
             )
 
