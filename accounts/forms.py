901df9e3a3a236b08ac04dfafeac47b37edd81f8
Vivek Narula <narula.vivek@gmail.com>
2025-12-27 14:57:59 +0530

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,41 +3,24 @@ from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
 
-from .models import User, BankAccountType, UserBankAccount, UserAddress
+from .models import User, BankAccountType, UserBankAccount
 from .constants import GENDER_CHOICE
-
-
-class UserAddressForm(forms.ModelForm):
-
-    class Meta:
-        model = UserAddress
-        fields = [
-            'street_address',
-            'city',
-            'postal_code',
-            'country'
-        ]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        for field in self.fields:
-            self.fields[field].widget.attrs.update({
-                'class': (
-                    'appearance-none block w-full bg-gray-200 '
-                    'text-gray-700 border border-gray-200 rounded '
-                    'py-3 px-4 leading-tight focus:outline-none '
-                    'focus:bg-white focus:border-gray-500'
-                )
-            })
 
 
 class UserRegistrationForm(UserCreationForm):
     account_type = forms.ModelChoiceField(
-        queryset=BankAccountType.objects.all()
+        queryset=BankAccountType.objects.all(),
+        required=True,
+        empty_label="---------"
     )
-    gender = forms.ChoiceField(choices=GENDER_CHOICE)
-    birth_date = forms.DateField()
+    gender = forms.ChoiceField(
+        choices=GENDER_CHOICE,
+        required=True
+    )
+    birth_date = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date"})
+    )
 
     class Meta:
         model = User
@@ -45,13 +28,15 @@ class UserRegistrationForm(UserCreationForm):
             'first_name',
             'last_name',
             'email',
+            'account_type',   # ✅ ADD
+            'gender',         # ✅ ADD
+            'birth_date',     # ✅ ADD
             'password1',
             'password2',
         ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         for field in self.fields:
             self.fields[field].widget.attrs.update({
                 'class': (
@@ -66,21 +51,17 @@ class UserRegistrationForm(UserCreationForm):
     @transaction.atomic
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user.username = None  # since email is USERNAME_FIELD
+
         if commit:
             user.save()
-            account_type = self.cleaned_data.get('account_type')
-            gender = self.cleaned_data.get('gender')
-            birth_date = self.cleaned_data.get('birth_date')
 
             UserBankAccount.objects.create(
                 user=user,
-                gender=gender,
-                birth_date=birth_date,
-                account_type=account_type,
-                account_no=(
-                    user.id +
-                    settings.ACCOUNT_NUMBER_START_FROM
-                )
+                account_type=self.cleaned_data['account_type'],
+                gender=self.cleaned_data['gender'],
+                birth_date=self.cleaned_data.get('birth_date'),
+                account_no=user.id + settings.ACCOUNT_NUMBER_START_FROM,
             )
+
         return user
