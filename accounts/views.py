aec13621d670cecd4eee5f4abb5b13c926be6d74
Vivek Narula <narula.vivek@gmail.com>
2025-12-27 15:09:30 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -4,71 +4,77 @@ from django.contrib.auth.views import LoginView
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, RedirectView
+from django.db import transaction
 
 from .forms import UserRegistrationForm, UserAddressForm
-
-
 
 User = get_user_model()
 
 
 class UserRegistrationView(TemplateView):
-    model = User
-    form_class = UserRegistrationForm
-    template_name = 'accounts/user_registration.html'
+    template_name = "accounts/user_registration.html"
 
     def dispatch(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
+        if request.user.is_authenticated:
             return HttpResponseRedirect(
-                reverse_lazy('transactions:transaction_report')
+                reverse_lazy("transactions:transaction_report")
             )
         return super().dispatch(request, *args, **kwargs)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if "registration_form" not in context:
+            context["registration_form"] = UserRegistrationForm()
+
+        if "address_form" not in context:
+            context["address_form"] = UserAddressForm()
+
+        return context
+
+    @transaction.atomic
     def post(self, request, *args, **kwargs):
-        registration_form = UserRegistrationForm(self.request.POST)
-        address_form = UserAddressForm(self.request.POST)
+        registration_form = UserRegistrationForm(request.POST)
+        address_form = UserAddressForm(request.POST)
 
         if registration_form.is_valid() and address_form.is_valid():
+            # Create user + bank account
             user = registration_form.save()
+
+            # Create address
             address = address_form.save(commit=False)
             address.user = user
             address.save()
 
-            login(self.request, user)
+            login(request, user)
+
             messages.success(
-                self.request,
+                request,
                 (
-                    f'Thank You For Creating A Bank Account. '
-                    f'Your Account Number is {user.account.account_no}. '
+                    "Thank you for creating a bank account. "
+                    f"Your Account Number is {user.account.account_no}."
                 )
             )
+
             return HttpResponseRedirect(
-                reverse_lazy('transactions:deposit_money')
+                reverse_lazy("transactions:deposit_money")
             )
 
         return self.render_to_response(
-            self.get_context_data(
-                registration_form=registration_form,
-                address_form=address_form
-            )
+            {
+                "registration_form": registration_form,
+                "address_form": address_form,
+            }
         )
-
-    def get_context_data(self, **kwargs):
-        if 'registration_form' not in kwargs:
-            kwargs['registration_form'] = UserRegistrationForm()
-        if 'address_form' not in kwargs:
-            kwargs['address_form'] = UserAddressForm()
-
-        return super().get_context_data(**kwargs)
 
 
 class UserLoginView(LoginView):
-    template_name='accounts/user_login.html'
+    template_name = "accounts/user_login.html"
     redirect_authenticated_user = True
 
 
 class LogoutView(RedirectView):
-    pattern_name = 'home'
+    pattern_name = "home"
 
     def get_redirect_url(self, *args, **kwargs):
         if self.request.user.is_authenticated:
